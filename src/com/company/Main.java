e5148fed773c21ae4dd99c32c351fb1091accecf
Ksenia Galenko <ksusha-cooi@mail.ru>
2021-10-28 23:15:39 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/*
+Переобуваемся на лету
+*/
+
+public class Main {
+
+    public static int result;
+
+    public static void main(String[] args) {
+        byte correction = Byte.MAX_VALUE;
+        for (double fahrenheit = -459.67; fahrenheit < 451; fahrenheit += 40) {
+            correction *= fahrenheit;
+            System.out.println(correction);
+        }
+    }
+}
